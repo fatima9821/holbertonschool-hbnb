915650b4451d315b061fdf5175ec3432f7944e15
Moctar93 <moctartize93@gmail.com>
2024-06-12 08:46:06 +0200

    data manager file and interface persistance manager

---
+++ hbnb/persistance/i_persistance_manager.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+"""Interface contenant les declarations des methodes CRUD"""
+
+
+class IPersistenceManager(ABC):
+    @abstractmethod
+    def save(self, entity):
+        pass
+
+    @abstractmethod
+    def get(self, entity_id, entity_type):
+        pass
+
+    @abstractmethod
+    def update(self, entity):
+        pass
+
+    @abstractmethod
+    def delete(self, entity_id, entity_type):
+        pass
+
