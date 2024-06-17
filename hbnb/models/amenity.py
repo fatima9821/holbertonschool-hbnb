9b6fa48b315bc1d8759c11a76f66d1b62bed5e0a
Ivy1610 <8682@holbertonschool.com>
2024-06-17 11:27:18 +0200

    creation de base model

--- hbnb/models/amenity.py
+++ hbnb/models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ create amenity """
 from datetime import datetime
+from models.base_models import BaseModel
 import uuid
 
 
@@ -8,7 +9,7 @@ class Amenity:
     def __init__(self, name):
         super().__init__()
         if not name:
-            raise TypeError("id  and name are required")
+            raise TypeError("name are required")
         self.name = name
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
--- hbnb/models/place.py
+++ hbnb/models/place.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """ creation de la place """
-from .base_models import BaseModel
-
+from models.base_models import BaseModel
+from datetime import datetime
+from uuid import uuid4
 
 class Place(BaseModel):
-    def __init__(self, id, name, description, address, city, latitude,
-                 longitude, host, number_of_rooms, bathrooms,
+    def __init__(self,place_id, name, description, address, city_id, latitude,
+                 longitude, host_id, number_of_rooms, bathrooms,
                  price_per_night, max_guests):
         super().__init__()
-        self.id = ()
-        self.name = ()
-        self.description = ()
-        self.address = ()
-        self.city_id = ()
-        self.latitude = ()
-        self.longitude = ()
-        self.host_id = ()
-        self.number_of_rooms = ()
-        self.bathrooms = ()
-        self.price_per_night = ()
-        self.max_guests = ()
+        self.place_id = uuid4
+        self.name = name
+        self.description = description
+        self.address = address
+        self.city_id = uuid4
+        self.latitude = latitude
+        self.longitude = longitude
+        self.host_id = uuid4
+        self.number_of_rooms = number_of_rooms
+        self.bathrooms = bathrooms
+        self.price_per_night = price_per_night
+        self.max_guests = max_guests
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         self.reviews = []
