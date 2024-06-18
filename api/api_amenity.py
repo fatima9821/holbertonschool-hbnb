cbd36f29dd428aa0725cd6f485ee6bda0cf208ea
Moctar93 <moctartize93@gmail.com>
2024-06-18 07:29:23 +0200

    amenety api update

--- api/api_amenity.py
+++ api/api_amenity.py
@@ -1,48 +1,35 @@
-from flask import request
 from flask_restx import Namespace, Resource, fields
-from data_manager import DataManager
-from model.amenity import Amenity
-from datetime import datetime
-import uuid
+from persistence.DataManager import DataManager
 
+api = Namespace("amenities", description="Amenities related operations")
+
+amenity_model = api.model( "Amenity",
+        { "id": fields.String(required=True, description="amenity ID"),
+"name": fields.String(required=True, description="amenity name"),
+        },
+    )
+
+@api.route("/")
 class AmenityList(Resource):
+    @api.doc("list_amenities")
+    @api.marshal_list_with(amenity_model)
     def get(self):
-        amenities = AmenityModel.query.all()
-        return {'amenities': [amenity.json() for amenity in amenities]}
 
-    def post(self):
-        data = self.get_json()
-        new_amenity = AmenityModel(name=data['name'], description=data['description'])
-        new_amenity.save_to_db()
-        return {'message': 'Commodité créée avec succès'}, 201
+        """List all amenity"""
+        amenity = AmenityManager().getAmenity()
+        if not users:
+            return []
+        return [amenity.toJSON() for amenity in amentys]
 
-class Amenity(Resource):
-    def get(self, amenity_id):
-        amenity = AmenityModel.query.get(amenity_id)
-        if amenity is None:
-            return {'message': 'Commodité introuvable'}, 404
-        return {'amenity': amenity.json()}
-
-    def put(self, amenity_id):
-        data = self.get_json()
-        amenity = AmenityModel.query.get(amenity_id)
-        if amenity is None:
-            return {'message': 'Commodité introuvable'}, 404
-        amenity.name = data.get('name')
-        amenity.description = data.get('description')
-        amenity.update_in_db()
-        return {'message': 'Commodité mise à jour avec succès'}
-
-    def delete(self, amenity_id):
-        amenity = AmenityModel.query.get(amenity_id)
-        if amenity is None:
-            return {'message': 'Commodité introuvable'}, 404
-        amenity.delete_from_db()
-        return {'message': 'Commodité supprimée avec succès'}
-
-api = Api(None)
-
-ns = api.namespace('', description='Opérations liées aux commodités')
-
-ns.add_resource(AmenityList, '/')
-ns.add_resource(Amenity, '/<int:amenity_id>')
+@api.route("/string:id")
+@api.param("id", "The Amenity identifier")
+@api.response(404, "Amenity not found")
+class AmenityRetrieve(Resource):
+    @api.doc("get_amenity")
+    @api.marshal_with(amenity_model)
+    def get(self, id):
+        """Fetch a user given its identifier"""
+        user = AmenitysManager().getAmenity(id)
+        if amenity:
+            return amenity.toJSON()
+        api.abort(404, "Amenity {} doesn't exist".format(id))
