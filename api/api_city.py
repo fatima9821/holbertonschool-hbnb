41c6ffd78d8548dd018f2d5403683ef92d554dda
Moctar93 <moctartize93@gmail.com>
2024-06-18 07:32:58 +0200

    update api amenity file

--- api/api_city.py
+++ api/api_city.py
@@ -1,25 +1,47 @@
-#!/usr/bin/python3
-
 from flask import request
 from flask_restx import Namespace, Resource, fields
 from data_manager import DataManager
 from model.city import City
 from datetime import datetime
 
+api = Namespace('cities', description='City related operations')
+data_manager = DataManager('data.json')
 
+city_model = api.model('City', {
+    'name': fields.String(required=True, description='City name'),
+    'country_id': fields.Integer(required=True, description='Country ID')
+})
+
+@api.route('/')
 class CityList(Resource):
+    @api.doc('list_cities')
     def get(self):
-        cities = CityModel.query.all()
-        return {'cities': [city.json() for city in cities]}
+        cities = [city.json() for city in data_manager.get_all(City)]
+        return {'cities': cities}, 200
 
+    @api.expect(city_model)
+    @api.doc('create_city')
     def post(self):
-        data = self.get_json()
-        new_city = CityModel(name=data['name'], country_id=data['country_id'])
-        new_city.save_to_db()
-        return {'message': 'Ville créée avec succès'}, 201
+        data = request.json
+        new_city = City(name=data['name'], country_id=data['country_id'])
+        data_manager.save(new_city)
+        return {'message': 'City created successfully'}, 201
 
+@api.route('/<int:city_id>')
+@api.response(404, 'City not found')
 class City(Resource):
+    @api.doc('get_city')
     def get(self, city_id):
-        city = CityModel.query.get(city_id)
+        city = data_manager.get(City, city_id)
         if city is None:
-            return {'message': 'Ville introuvable'}, 201
+            return {'message': 'City not found'}, 404
+        return city.json(), 200
+
+    @api.doc('delete_city')
+    def delete(self, city_id):
+        city = data_manager.get(City, city_id)
+        if city is None:
+            return {'message': 'City not found'}, 404
+        data_manager.delete(City, city_id)
+        return {'message': 'City deleted successfully'}, 204
+
